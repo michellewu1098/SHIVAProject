34e6d674a9ebffe45759347da99047cda4d3728e
Michelle Wu <michelle.wu1098@gmail.com>
2017-03-16 16:00:14 +0000

    Polishing shiva-metamorphosis

--- shiva-metamorphosis/src/main.cpp
+++ shiva-metamorphosis/src/main.cpp
@@ -1,4 +1,8 @@
-
+///-----------------------------------------------------------------------------------------------
+/// \file main.cpp
+/// \author Leigh McLoughlin
+/// \version 1.0
+///-----------------------------------------------------------------------------------------------
 
 #include "GUIManager.h"
 #include "ResourceManager.h"
@@ -10,81 +14,83 @@
 #include "RotationChooser.h"
 #include "ShivaMetamorphosis.h"
 
-//////////////////////////////////////////////////////////////////////////
-#include <iostream>
 #include <boost/program_options.hpp>
 
-//////////////////////////////////////////////////////////////////////////
-
-
-// This will be used for storing our program options, which may also be loaded by command-line
-struct program_options
+//----------------------------------------------------------------------------------
+/// \brief  This will be used for storing our program options, which may also be loaded by command-line
+//----------------------------------------------------------------------------------
+struct ProgramOptions
 {
 	std::string profileDirectory;
 	std::string profileName;
 };
 
+//----------------------------------------------------------------------------------
+
 // Forward declaration of a function that will sort out our command-line options
-bool get_options(program_options *options, int argc, char **argv);
 
+bool GetOptions( ProgramOptions *_options, int _argc, char **_argv );
 
+//----------------------------------------------------------------------------------
 
-int main(int argc, char **argv)
+int main( int argc, char **argv )
 {
 	// Set our program options from command line
-	program_options options;
-	if( !get_options(&options,argc,argv) )
+	ProgramOptions options;
+	if( !GetOptions( &options, argc, argv ) )
 		return 0;
 
-	ShivaModelManager::Init("Resources/Models/index.xml");
+	ShivaModelManager::Init( "Resources/Models/index.xml" );
 
-	ShivaGUI::GUIManager *mainGUIManager = new ShivaGUI::GUIManager("Metamorphosis Prototype 1b", "metamorph1b");
+	ShivaGUI::GUIManager *mainGUIManager = new ShivaGUI::GUIManager( "Metamorphosis Prototype 1b", "metamorph1b" );
 
-	mainGUIManager->RegisterActivityCreator("ShivaMetamorphosis",ShivaMetamorphosis::Factory);
-	mainGUIManager->RegisterActivityCreator("ModelChooser",ModelChooser::Factory);
-	mainGUIManager->RegisterActivityCreator("ColourChooser",ColourChooser::Factory);
-	mainGUIManager->RegisterActivityCreator("RotationChooser",RotationChooser::Factory);
-	mainGUIManager->RegisterViewCreator("SDFView",SDFView::Factory);
+	mainGUIManager->RegisterActivityCreator( "ShivaMetamorphosis", ShivaMetamorphosis::Factory );
+	mainGUIManager->RegisterActivityCreator( "ModelChooser", ModelChooser::Factory );
+	mainGUIManager->RegisterActivityCreator( "ColourChooser", ColourChooser::Factory );
+	mainGUIManager->RegisterActivityCreator( "RotationChooser", RotationChooser::Factory );
+	mainGUIManager->RegisterViewCreator( "SDFView", SDFView::Factory );
 
-	mainGUIManager->StartWithProfileChooser("ModelChooser",options.profileDirectory,options.profileName);
+	mainGUIManager->StartWithProfileChooser( "ModelChooser", options.profileDirectory, options.profileName );
 
 	return 0;
 }
 
-// This function will deal with our command line options, setting up the profile directory and profile name
-bool get_options(program_options *options, int argc, char **argv)
+//----------------------------------------------------------------------------------
+
+/// \brief This function will deal with our command line options, setting up the profile directory and profile name
+bool GetOptions( ProgramOptions *_options, int _argc, char **_argv )
 {
-	boost::program_options::options_description generic("Generic options");
+	boost::program_options::options_description generic( "Generic options" );
 	generic.add_options()
-		("version,v", "print version string")
-		("help,h", "produce help message");
+		( "version,v", "print version string" )
+		( "help,h", "produce help message" );
 
-	boost::program_options::options_description profile("Profile options");
+	boost::program_options::options_description profile( "Profile options" );
 	profile.add_options()
-		("profile_directory,d",
-		 boost::program_options::value<std::string>(&(options->profileDirectory))->default_value("Profiles"),
-		 "profile directory")
-		("profile_name,n",
-		 boost::program_options::value<std::string>(&(options->profileName))->default_value(""),
-		 "profile name");
+		( "profile_directory,d",
+		 boost::program_options::value< std::string >( &( _options->profileDirectory ) )->default_value( "Profiles" ),
+		 "profile directory" )
+		( "profile_name,n",
+		 boost::program_options::value< std::string >( &( _options->profileName ) )->default_value( "" ),
+		 "profile name" );
 
 
-	boost::program_options::options_description allOptions("Allowed options");
-	allOptions.add(generic).add(profile);
+	boost::program_options::options_description allOptions( "Allowed options" );
+	allOptions.add( generic ).add( profile );
 
 	boost::program_options::variables_map variableMap;
-	boost::program_options::store(boost::program_options::parse_command_line(argc, argv, allOptions), variableMap);
-	boost::program_options::notify(variableMap);
+	boost::program_options::store( boost::program_options::parse_command_line( _argc, _argv, allOptions ), variableMap );
+	boost::program_options::notify( variableMap );
 
 	// See what we have found in our command line options:
-	if( variableMap.count("help") )
+	if( variableMap.count( "help" ) )
 	{
 		// This will print out a nicely formatted help text of all our options
 	    std::cout << allOptions << std::endl;
 	    // Return false to exit the program
 	    return false;
 	}
-	else if( variableMap.count("version") )
+	else if( variableMap.count( "version" ) )
 	{
 	    std::cout << "SHIVA Metamorphosis Exercise version 2.1" << std::endl;
 	    // Return false to exit the program
@@ -93,3 +99,5 @@ bool get_options(program_options *options, int argc, char **argv)
 
 	return true;
 }
+
+//----------------------------------------------------------------------------------
